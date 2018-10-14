b3d84d2668b03b9375d066829957066e3935c42d
Florian Stellbrink <florian@stellbr.ink>
2018-10-14 17:09:37 +0200

    Clean up skeleton

--- mycoco.py
+++ mycoco.py
@@ -6,23 +6,23 @@ import sys
 
 # This is evil, forgive me, but practical under the circumstances.
 # It's a hardcoded access to the COCO API.  
-COCOAPI_PATH='/scratch/lt2316-h18-resources/cocoapi/PythonAPI/'
-TRAIN_ANNOT_FILE='/scratch/lt2316-h18-resources/coco/annotations/instances_train2017.json'
-VAL_ANNOT_FILE='/scratch/lt2316-h18-resources/coco/annotations/instances_val2017.json'
-TRAIN_CAP_FILE='/scratch/lt2316-h18-resources/coco/annotations/captions_train2017.json'
-VAL_CAP_FILE='/scratch/lt2316-h18-resources/coco/annotations/captions_val2017.json'
-TRAIN_IMG_DIR='/scratch/lt2316-h18-resources/coco/train2017/'
-VAL_IMG_DIR='/scratch/lt2316-h18-resources/coco/val2017/'
-annotfile = TRAIN_ANNOT_FILE
-capfile = TRAIN_CAP_FILE
-imgdir = TRAIN_IMG_DIR
+COCOAPI_PATH = '/scratch/lt2316-h18-resources/cocoapi/PythonAPI/'
+TRAIN_ANNOTATION_FILE = '/scratch/lt2316-h18-resources/coco/annotations/instances_train2017.json'
+VALIDATION_ANNOTATION_FILE = '/scratch/lt2316-h18-resources/coco/annotations/instances_val2017.json'
+TRAIN_CAPTION_FILE = '/scratch/lt2316-h18-resources/coco/annotations/captions_train2017.json'
+VAL_CAPTION_FILE = '/scratch/lt2316-h18-resources/coco/annotations/captions_val2017.json'
+TRAIN_IMAGE_DIR = '/scratch/lt2316-h18-resources/coco/train2017/'
+VALIDATION_IMAGE_DIR = '/scratch/lt2316-h18-resources/coco/val2017/'
+annotation_file = TRAIN_ANNOTATION_FILE
+caption_file = TRAIN_CAPTION_FILE
+image_directory = TRAIN_IMAGE_DIR
 
 sys.path.append(COCOAPI_PATH)
 from pycocotools.coco import COCO
 
-annotcoco = None
-capcoco = None
-catdict = {}
+annotation_coco = None
+caption_coco = None
+category_dictionary = {}
 
 # OK back to normal.
 import random
@@ -30,97 +30,101 @@ import skimage.io as io
 import skimage.transform as tform
 import numpy as np
 
+
 def setmode(mode):
-    '''
+    """
     Set entire module's mode as 'train' or 'test' for the purpose of data extraction.
-    '''
-    global annotfile
-    global capfile
-    global imgdir
-    global annotcoco, capcoco
-    global catdict
+    """
+    global annotation_file
+    global caption_file
+    global image_directory
+    global annotation_coco, caption_coco
+    global category_dictionary
     if mode == "train":
-        annotfile = TRAIN_ANNOT_FILE
-        capfile = TRAIN_CAP_FILE
-        imgdir = TRAIN_IMG_DIR
+        annotation_file = TRAIN_ANNOTATION_FILE
+        caption_file = TRAIN_CAPTION_FILE
+        image_directory = TRAIN_IMAGE_DIR
     elif mode == "test":
-        annotfile = VAL_ANNOT_FILE
-        capfile = VAL_CAP_FILE
-        imgdir = VAL_IMG_DIR
+        annotation_file = VALIDATION_ANNOTATION_FILE
+        caption_file = VAL_CAPTION_FILE
+        image_directory = VALIDATION_IMAGE_DIR
     else:
         raise ValueError
 
-    annotcoco = COCO(annotfile)
-    capcoco = COCO(capfile)
+    annotation_coco = COCO(annotation_file)
+    caption_coco = COCO(caption_file)
 
     # To facilitate category lookup.
-    cats = annotcoco.getCatIds()
-    catdict = {x:(annotcoco.loadCats(ids=[x])[0]['name']) for x in cats}
-    
+    cats = annotation_coco.getCatIds()
+    category_dictionary = {x: (annotation_coco.loadCats(ids=[x])[0]['name']) for x in cats}
+
+
 def query(queries, exclusive=True):
-    '''
-    Collects mutually-exclusive lists of COCO ids by queries, so returns 
+    """
+    Collects mutually-exclusive lists of COCO ids by queries, so returns
     a parallel list of lists.
-    (Setting 'exclusive' to False makes the lists non-exclusive.)  
+    (Setting 'exclusive' to False makes the lists non-exclusive.)
     e.g., exclusive_query([['toilet', 'boat'], ['umbrella', 'bench']])
     to find two mutually exclusive lists of images, one with toilets and
     boats, and the other with umbrellas and benches in the same image.
-    '''
-    if not annotcoco:
+    """
+    if not annotation_coco:
         raise ValueError
-    imgsets = [set(annotcoco.getImgIds(catIds=annotcoco.getCatIds(catNms=x))) for x in queries]
+    image_sets = [set(annotation_coco.getImgIds(catIds=annotation_coco.getCatIds(catNms=x))) for x in queries]
     if len(queries) > 1:
         if exclusive:
-            common = set.intersection(*imgsets)
-            return [[x for x in y if x not in common] for y in imgsets]
+            common = set.intersection(*image_sets)
+            return [[x for x in y if x not in common] for y in image_sets]
         else:
-            return [list(y) for y in imgsets]
+            return [list(y) for y in image_sets]
     else:
-        return [list(imgsets[0])]
-    
-def get_captions_for_ids(idlist):
-    annids =  capcoco.getAnnIds(imgIds=idlist)
-    anns = capcoco.loadAnns(annids)
-    return [ann['caption'] for ann in anns]    
+        return [list(image_sets[0])]
 
-def get_cats_for_img(imgid):
-    '''
+
+def get_captions_for_ids(id_list):
+    annotation_ids = caption_coco.getAnnIds(imgIds=id_list)
+    annotations = caption_coco.loadAnns(annotation_ids)
+    return [ann['caption'] for ann in annotations]
+
+
+def get_cats_for_img(image_id):
+    """
     Takes an image id and gets a category list for it.
-    '''
-    if not annotcoco:
+    """
+    if not annotation_coco:
         raise ValueError
-        
-    imgannids = annotcoco.getAnnIds(imgIds=imgid)
-    imganns = annotcoco.loadAnns(imgannids)
-    return list(set([catdict[x['category_id']] for x in imganns]))
-    
-    
-def iter_captions(idlists, cats, batch=1):
-    '''
+
+    image_nn_ids = annotation_coco.getAnnIds(imgIds=image_id)
+    image_nns = annotation_coco.loadAnns(image_nn_ids)
+    return list(set([category_dictionary[x['category_id']] for x in image_nns]))
+
+
+def iter_captions(id_lists, categories, batch=1):
+    """
     Obtains the corresponding captions from multiple COCO id lists.
-    Randomizes the order.  
+    Randomizes the order.
     Returns an infinite iterator (do not convert to list!) that returns tuples (captions, categories)
     as parallel lists at size of batch.
-    '''
-    if not capcoco:
+    """
+    if not caption_coco:
         raise ValueError
     if batch < 1:
         raise ValueError
 
     full = []
-    for z in zip(idlists, cats):
+    for z in zip(id_lists, categories):
         for x in z[0]:
             full.append((x, z[1]))
-        
+
     while True:
-        randomlist = random.sample(full, k=len(full))
+        random_list = random.sample(full, k=len(full))
         captions = []
         labels = []
 
-        for p in randomlist:
-            annids =  capcoco.getAnnIds(imgIds=[p[0]])
-            anns = capcoco.loadAnns(annids)
-            for ann in anns:
+        for p in random_list:
+            annotation_ids = caption_coco.getAnnIds(imgIds=[p[0]])
+            annotations = caption_coco.loadAnns(annotation_ids)
+            for ann in annotations:
                 captions.append(ann['caption'])
                 # For LSTM you may want to do more with the captions
                 # or otherwise distribute the data.
@@ -130,34 +134,35 @@ def iter_captions(idlists, cats, batch=1):
                     captions = []
                     labels = []
 
-def iter_captions_cats(idlists, cats, batch=1):
-    '''
+
+def iter_captions_cats(id_lists, cats, batch=1):
+    """
     Obtains the corresponding captions from multiple COCO id lists alongside all associated image captions per image.
-    Randomizes the order.  
+    Randomizes the order.
     Returns an infinite iterator (do not convert to list!) that returns tuples (captions, categories)
     as parallel lists at size of batch.
-    '''
-    if not capcoco:
+    """
+    if not caption_coco:
         raise ValueError
     if batch < 1:
         raise ValueError
 
     full = []
-    for z in zip(idlists, cats):
+    for z in zip(id_lists, cats):
         for x in z[0]:
             full.append((x, z[1]))
-        
+
     while True:
-        randomlist = random.sample(full, k=len(full))
+        random_list = random.sample(full, k=len(full))
         captions = []
         labels = []
 
-        for p in randomlist:
-            annids =  capcoco.getAnnIds(imgIds=[p[0]])
-            anns = capcoco.loadAnns(annids)
-            for ann in anns:
-                imgid = ann['image_id']
-                cats = get_cats_for_img(imgid)
+        for p in random_list:
+            annotation_ids = caption_coco.getAnnIds(imgIds=[p[0]])
+            annotations = caption_coco.loadAnns(annotation_ids)
+            for ann in annotations:
+                image_id = ann['image_id']
+                cats = get_cats_for_img(image_id)
                 captions.append((ann['caption'], cats))
                 # For LSTM you may want to do more with the captions
                 # or otherwise distribute the data.
@@ -166,38 +171,39 @@ def iter_captions_cats(idlists, cats, batch=1):
                     yield (captions, labels)
                     captions = []
                     labels = []
-                    
-def iter_images(idlists, cats, size=(200,200), batch=1):
-    '''
+
+
+def iter_images(id_lists, cats, size=(200, 200), batch=1):
+    """
     Obtains the corresponding image data as numpy array from multiple COCO id lists.
     Returns an infinite iterator (do not convert to list!) that returns tuples (imagess, categories)
     as parallel lists at size of batch.
     By default, randomizes the order and resizes the image.
-    '''
-    if not annotcoco:
+    """
+    if not annotation_coco:
         raise ValueError
     if batch < 1:
         raise ValueError
     if not size:
-        raise ValueError # size is mandatory
+        raise ValueError  # size is mandatory
 
     full = []
-    for z in zip(idlists, cats):
+    for z in zip(id_lists, cats):
         for x in z[0]:
             full.append((x, z[1]))
 
     while True:
-        randomlist = random.sample(full, k=len(full))
+        random_list = random.sample(full, k=len(full))
 
         images = []
         labels = []
-        for r in randomlist:
-            imgfile = annotcoco.loadImgs([r[0]])[0]['file_name']
-            img = io.imread(imgdir + imgfile)
-            imgscaled = tform.resize(img, size)
+        for r in random_list:
+            image_file = annotation_coco.loadImgs([r[0]])[0]['file_name']
+            image = io.imread(image_directory + image_file)
+            image_scaled = tform.resize(image, size)
             # Colour images only.
-            if imgscaled.shape == (size[0], size[1], 3):
-                images.append(imgscaled)
+            if image_scaled.shape == (size[0], size[1], 3):
+                images.append(image_scaled)
                 labels.append(r[1])
                 if len(images) % batch == 0:
                     yield (np.array(images), np.array(labels))
--- train.py
+++ train.py
@@ -3,27 +3,32 @@
 # You can create whatever additional modules and helper scripts you need,
 # as long as all the training functionality can be reached from this script.
 
-# Add/update whatever imports you need.
 from argparse import ArgumentParser
 import mycoco
 
-# If you do option A, you may want to place your code here.  You can
-# update the arguments as you need.
-def optA():
+
+def opt_a():
+    """
+    Option A - Convolutional image autoencoder
+    """
     mycoco.setmode('train')
     print("Option A not implemented!")
 
-# If you do option B, you may want to place your code here.  You can
-# update the arguments as you need.
-def optB():
+
+def opt_b():
+    """
+    Option B - Multi-task caption predictor/classifier
+    """
     mycoco.setmode('train')
     print("Option B not implemented!")
 
-# Modify this as needed.
+
 if __name__ == "__main__":
-    parser = ArgumentParser("Train a model.")    
+    parser = ArgumentParser("Train a model.")
+
     # Add your own options as flags HERE as necessary (and some will be necessary!).
     # You shouldn't touch the arguments below.
+
     parser.add_argument('-P', '--option', type=str,
                         help="Either A or B, based on the version of the assignment you want to run. (REQUIRED)",
                         required=True)
@@ -43,7 +48,7 @@ if __name__ == "__main__":
 
     if len(args.categories) < 2:
         print("Too few categories (<2).")
-        exit(0)
+        exit(1)
 
     print("The queried COCO categories are:")
     for c in args.categories:
@@ -51,9 +56,9 @@ if __name__ == "__main__":
 
     print("Executing option " + args.option)
     if args.option == 'A':
-        optA()
+        opt_a()
     elif args.option == 'B':
-        optB()
+        opt_b()
     else:
         print("Option does not exist.")
-        exit(0)
+        exit(1)
